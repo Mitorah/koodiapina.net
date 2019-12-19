cf50fb2db5497f98adaf9bf5aeb513b4bbd18a75
Mitorah <leinonen.op@gmail.com>
2019-12-19 20:42:20 +0200

    empty commit

--- helloworld.js
+++ helloworld.js
@@ -1,14 +1,17 @@
-<!DOCTYPE HTML>
 <html>
-    <body>
-        <p>Before the script.....</p> 
-        
-        <script>
-            alert('Hello World!');
-        </script>
 
-        <p>After the script...</p>
+<body>
+
+  <p>Before the script...</p>
+
+  <script>
+    alert( 'Hello, world!' );
+  </script>
+
+  <p>...After the script.</p>
+
+</body>
 
 
-    </body>
+
 </html>
