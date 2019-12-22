f79becb69bc396282449957accef9fdf84048dcd
Mitorah <leinonen.op@gmail.com>
2019-12-22 21:42:44 +0200

    Fixed missing semicolon.

--- w3school_examples/helloworld.js
+++ w3school_examples/helloworld.js
@@ -4,5 +4,5 @@ var dt = require('./myfirstmodule');
 http.createServer(function (req, res) {
   res.writeHead(200,{'Content-Type': 'text/html'});
   res.write("The date and time currently: ", dt.myDateTime());
-  res.end('Hello world!');
+  res.end();
 }).listen(8080);
