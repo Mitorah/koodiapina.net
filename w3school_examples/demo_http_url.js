80dfe73af8d025d094c71a81409b1ee5d6c5ec82
Mitorah <leinonen.op@gmail.com>
2019-12-22 22:09:47 +0200

    Fixed demo_htt_url -createserver.

--- w3school_examples/demo_http_url.js
+++ w3school_examples/demo_http_url.js
@@ -1,7 +1,7 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
-    res.writeHead(200, { 'text/html' });
-    res.writeHead(req.url);
+    res.writeHead(200, {'Content-type': 'text/html' });
+    res.write(req.url);
     res.end();
 }).listen(8080);
