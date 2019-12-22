730556f6e1f4ff47e5f3ac0ff9f03aba43d71758
Mitorah <leinonen.op@gmail.com>
2019-12-22 22:32:00 +0200

    Fixed writeHead in createserver.

--- w3school_examples/demo_querystring.js
+++ w3school_examples/demo_querystring.js
@@ -2,7 +2,7 @@ var http = require('http');
 var url = require('url');
 
 http.createServer(function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'html/text'});
+    res.writeHead(200, { 'Content-Type': 'text/html'});
     var q = url.parse(req.url, true).query;
     var txt = q.year + " " + q.month;
     res.end(txt);
