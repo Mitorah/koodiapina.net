c6b32ff49a3aaccc4f88ea9ba05cbe2d79aa3b08
Mitorah <leinonen.op@gmail.com>
2019-12-24 21:25:56 +0200

    Fixed demo_uploadfile.js

--- w3school_examples/demo_uploadfile.js
+++ w3school_examples/demo_uploadfile.js
@@ -2,7 +2,7 @@ var http = require('http');
 var formidable = require('formidable');
 
 http.createServer(function (req, res) {
-    if (req.url = '/fileupload') {
+    if (req.url == '/fileupload') {
         var form = new formidable.IncomingForm();
         form.parse(req, function(err, fields, files) {
             res.write('File uploaded.');
