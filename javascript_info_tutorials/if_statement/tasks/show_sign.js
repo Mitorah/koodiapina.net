7860c884a92e3c92d9c8b3e09c1938248e79c432
Mitorah <leinonen.op@gmail.com>
2020-01-06 14:40:03 +0200

    Result should now correctly create an alert.

--- javascript_info_tutorials/if_statement/tasks/show_sign.js
+++ javascript_info_tutorials/if_statement/tasks/show_sign.js
@@ -15,6 +15,6 @@ while (true)
         break;
     }
     else if (result == NaN) {
-        alert(result + `is not a number! I need a number!`)
+        alert(`${result} is not a number! I need a number!`)
     }
 }
