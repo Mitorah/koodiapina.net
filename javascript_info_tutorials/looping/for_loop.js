963351f2cc35eb1af2d7fefef1aa36d395466c6b
Mitorah <leinonen.op@gmail.com>
2020-01-06 19:09:48 +0200

    Added for_loop-tutorials.

---
+++ javascript_info_tutorials/looping/for_loop.js
@@ -0,0 +1,21 @@
+/*
+- Use the for loop to output even numbers from 2 to 10.
+*/
+
+alert(`Use the for loop to output even numbers from 2 to 10.`)
+
+for (let i = 2; i < 10; i++) {
+    if (i % 2 == 1) 
+        alert(i);
+}
+
+/*
+- Rewrite the code changing the for loop to while without altering its behavior (the output should stay same).
+*/
+
+alert(`Rewrite the code changing the for loop to while without altering its behavior (the output should stay same).`);
+
+while (i++ < 10) {
+    if (i % 2 == 1)
+        alert(i);
+}
