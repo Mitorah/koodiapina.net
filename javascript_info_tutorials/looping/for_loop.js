5c1372322f6e9c5a2ee294121f56b197f6311dc0
Mitorah <leinonen.op@gmail.com>
2020-01-06 19:11:03 +0200

    Corrected if-clause.

--- javascript_info_tutorials/looping/for_loop.js
+++ javascript_info_tutorials/looping/for_loop.js
@@ -5,7 +5,7 @@
 alert(`Use the for loop to output even numbers from 2 to 10.`)
 
 for (let i = 2; i < 10; i++) {
-    if (i % 2 == 1) 
+    if (i % 2 == 0) 
         alert(i);
 }
 
@@ -16,6 +16,6 @@ for (let i = 2; i < 10; i++) {
 alert(`Rewrite the code changing the for loop to while without altering its behavior (the output should stay same).`);
 
 while (i++ < 10) {
-    if (i % 2 == 1)
+    if (i % 2 == 0)
         alert(i);
 }
