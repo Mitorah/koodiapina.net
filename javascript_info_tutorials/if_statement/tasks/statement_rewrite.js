e60ed14ee3e2ae8a3b0dcbf58b6eae6841859588
Mitorah <leinonen.op@gmail.com>
2020-01-06 14:54:45 +0200

    Added correct variables to statement_rewrite.js

--- javascript_info_tutorials/if_statement/tasks/statement_rewrite.js
+++ javascript_info_tutorials/if_statement/tasks/statement_rewrite.js
@@ -7,6 +7,8 @@ if (a + b < 4) {
   result = 'Over';
 }
 */
+let a = prompt('Give me a number(a):');
+let b = prompt('Give me number(b):')
 
-let result = (a + b < 4 ) ? 'Below' : 'Over';
+let result = (a + b < 4 ) ? `A + B is below 4 (${a+b})` : `A + B is equal or over 4 (${a+b})`;
 alert(result);
