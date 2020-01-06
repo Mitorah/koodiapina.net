bea2bc66728db5d4a5935e9811367620c7b88901
Mitorah <leinonen.op@gmail.com>
2020-01-06 14:35:06 +0200

    Added show sign tutorial.

---
+++ javascript_info_tutorials/if_statement/tasks/show_sign.js
@@ -0,0 +1,20 @@
+while (true)
+{
+    let result = prompt(`Give me a number.`);
+    
+    if (result > 0) {
+        alert('1');
+        break;
+    }
+    else if (result < 0) {
+        alert('-1');
+        break;
+    }
+    else if (result == 0) {
+        alert('0');
+        break;
+    }
+    else if (result == NaN) {
+        alert(result + `is not a number! I need a number!`)
+    }
+}
