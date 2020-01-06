05a5e177d1d0e2c45f598ab4e6d875e5028f44d5
Mitorah <leinonen.op@gmail.com>
2020-01-06 14:29:14 +0200

    Added statement tutorial.

---
+++ javascript_info_tutorials/if_statement/tasks/name_of_javascript.js
@@ -0,0 +1,8 @@
+let result = prompt(`What's the "official" name of JavaScript?`)
+
+if (result == "ECMAScript") {
+    alert('"Right!');
+}
+else {
+    alert(`You don't know? "ECMAScript"!`)
+    }
