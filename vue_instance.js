7fba317a27b7eee59131a2bd15f4c2d98e0866c1
Mitorah <leinonen.op@gmail.com>
2020-02-09 16:45:46 +0200

    Added htmlcontent data.

--- vue_instance.js
+++ vue_instance.js
@@ -2,13 +2,13 @@ var vm = new Vue({
     el: "#vue_det",
     data: {
         first_name: "John",
-        last_name: "Doe"
+        last_name: "Doe",
+        htmlcontent: "<div><h1>Vue Js Template</h1></div>"
     },
 
     methods: {
         information : function() {
             return "My name is " + this.first_name + " " + this.last_name + ".";
         }
-
     }
 })
