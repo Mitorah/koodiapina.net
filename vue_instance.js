90750b2657a57b9ff4c075049cd7d55cf3b66c3e
Mitorah <leinonen.op@gmail.com>
2020-02-09 19:30:29 +0200

    Added mouseover.

--- vue_instance.js
+++ vue_instance.js
@@ -4,12 +4,20 @@ var vm = new Vue({
         first_name: "John",
         last_name: "Doe",
         htmlcontent: "<div><h1>Vue Js Template</h1></div>",
-        imgsrc: "images/bowling_beer.jpg"
+        visible_img: "",
+        imgsrc_1: "images/bowling_beer.jpg",
+        imgsrc_2: "images/hots_alarak.jpg"
     },
 
     methods: {
         information : function() {
             return "My name is " + this.first_name + " " + this.last_name + ".";
+        },
+        changetoimg_1: function() {
+            visible_img = imgsrc_1;
+        },
+        changetoimg_2: function() {
+            visible_img = imgsrc_2;
         }
     }
 })
