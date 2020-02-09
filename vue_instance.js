776d91735a57b0141f6a0269de3463239c93a666
Mitorah <leinonen.op@gmail.com>
2020-02-09 19:50:23 +0200

    image

--- vue_instance.js
+++ vue_instance.js
@@ -4,7 +4,7 @@ var vm = new Vue({
         first_name: "John",
         last_name: "Doe",
         htmlcontent: "<div><h1>Vue Js Template</h1></div>",
-        visible_img: "",
+        visible_img: "images/hots_alarak.jpg",
         imgsrc_1: "images/bowling_beer.jpg",
         imgsrc_2: "images/hots_alarak.jpg",
         imgsize: 50
