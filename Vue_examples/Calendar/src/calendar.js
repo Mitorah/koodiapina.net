c04fb1196b3a10761a2415673d6757496c533ed1
Mitorah <leinonen.op@gmail.com>
2020-02-16 14:30:13 +0200

    Removed data: function()

--- Vue_examples/Calendar/src/calendar.js
+++ Vue_examples/Calendar/src/calendar.js
@@ -1,10 +1,10 @@
 Vue.component('meeting_visual_data', {
     template: '<h5>{{ item }}<button v-on:click = "delete_button(item)">Delete</button></h5><br/>',
-    data: function() {
+    /*data: function() {
         return {
             counter: 0
         }
-    },
+    },*/
     props: ['item'],
     methods: {
         delete_button: function(mt) {
