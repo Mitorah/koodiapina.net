33e1f9d81489d2e0cb2266f30c937c28c0b6031e
Mitorah <leinonen.op@gmail.com>
2020-02-16 12:35:38 +0200

    Added visualisation template.

--- Vue_examples/Calendar/src/calendar.js
+++ Vue_examples/Calendar/src/calendar.js
@@ -1,3 +1,18 @@
+Vue.Component('meeting_visual_data', {
+    template: '<h5>{{ item }}</h5><br/>',
+    data: function() {
+        return {
+            counter: 0
+        }
+    },
+    props: ['item'],
+    methods: {
+        display_meeting: function(mt) {
+            this.$emit('showmeeting', mt);
+        }
+    },
+});
+
 var cal = new Vue({
    el: '#calendar',
    data: {
