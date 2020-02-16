062514e482227f7609f205319d72bbd180571147
Mitorah <leinonen.op@gmail.com>
2020-02-16 19:20:39 +0200

    Added calendar example.

--- Vue_examples/Calendar/src/calendar.js
+++ Vue_examples/Calendar/src/calendar.js
@@ -11,13 +11,25 @@ Vue.component('meeting_visual_data', {
 var cal = new Vue({
    el: '#calendar',
    data: {
-       new_meeting: null,
-       meetings: [Date],
+       new_meeting_id: "",
+       new_meeting_date: null,
+       new_meeting_start_time: null,
+       new_meeting_end_time: null,
+       meetings: [[]],
    },
    methods: {
        addmeeting: function() {
-           this.meetings.push(this.new_meeting)
-           this.new_meeting = null
+            this.meetings.push({
+                name: this.new_meeting_id,
+                date: this.new_meeting_date,
+                start_time: this.new_meeting_start_time,
+                end_time: this.new_meeting_end_time,
+            });
+
+            this.new_meeting_id = ""
+            this.new_meeting_date = null
+            this.new_meeting_start_time = null
+            this.new_meeting_end_time = null
        },
        deletemeeting: function(deletedmeeting) {
            this.meetings.splice(this.meetings.indexOf(deletedmeeting), 1);
@@ -25,3 +37,4 @@ var cal = new Vue({
    },
 
 });
+
