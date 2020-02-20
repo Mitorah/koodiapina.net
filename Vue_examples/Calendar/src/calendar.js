60fa290aae8f7530002f690135ffed1433a1ad82
Mitorah <leinonen.op@gmail.com>
2020-02-20 19:25:53 +0200

    Added visual_data_component.js as its own file.

--- Vue_examples/Calendar/src/calendar.js
+++ Vue_examples/Calendar/src/calendar.js
@@ -1,14 +1,3 @@
-Vue.component('meeting_visual_data', {
-    template: '<h5>{{' + 
-    'item.name }}: {{item.start_time}} - {{item.end_time}}, {{item.date}} <button v-on:click = "delete_button(item)">Delete</button></h5><br/>',
-    props: ['item'],
-    methods: {
-        delete_button: function(mt) {
-            this.$emit('delete_meeting', mt);
-        }
-    },
-});
-
 var cal = new Vue({
    el: '#calendar',
    data: {
