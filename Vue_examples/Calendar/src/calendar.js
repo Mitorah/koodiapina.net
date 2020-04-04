c3ab90c95406c2c292159b87d66bbb5c2a13a68f
Mitorah <leinonen.op@gmail.com>
2020-04-04 20:51:39 +0300

    Reconstructing Koodiapina.net.

--- Vue_examples/Calendar/src/calendar.js
+++ Vue_examples/Calendar/src/calendar.js
@@ -1,73 +1,62 @@
-var cal = new Vue({
-   el: '#calendar',
+var cal = new Vue ({
+    el: '#calendar',
    data: {
-       new_user_id: "",
+       users: [
+            {
+                user_id: "First user",
+                meetings: [],
+            },
+            {
+                user_id: "Second user",
+                meetings: [],
+            }
+       ],
 
+       new_user_id: "",
+       
        new_meeting_id: "",
        new_meeting_date: null,
        new_meeting_start_time: null,
        new_meeting_end_time: null,
-
+       
        selectedperson: "",
-       personalmeetings: [
+    },
+    methods: {
+        getpersonalmeetings: function(userid) {
+            var selecteduser = this.getselectedperson(userid);
+
+            if (selecteduser != null)
+                return selecteduser.meetings;
+        },
+        getselectedperson: function(userid) {
+           for (var i = 0; i < this.users.length; i++)
            {
-               id: "First person",
-               pmeeting: [], 
-           },
-           {
-               id: "Second person",
-               pmeeting: [],
-           }
-        ]
-   },
-   methods: {
-       getpersonalmeetings: function() {
-            var selectedperson = this.getselectedperson();
-            
-            if (selectedperson != null)
-                return selectedperson.pmeeting;
-       },
-       getselectedperson: function() {
-           for (var i = 0; i < this.personalmeetings.length; i++)
-           {
-               if (this.personalmeetings[i].id == this.selectedperson) {
-                   return this.personalmeetings[i];
+               if (this.users[i].user_id == userid) {
+                   return this.users[i];
                }
            }
-       },
-       addpersonalmeeting: function() {
-           var user = this.getselectedperson();
-           
-            user.pmeeting.push({
-            name: this.new_meeting_id,
-            date: this.new_meeting_date,
-            start_time: this.new_meeting_start_time,
-            end_time: this.new_meeting_end_time,
+        },
+        addpersonalmeeting: function(userid, newid, newdate, starttime, endtime) {
+            this.getselectedperson(userid).meetings.push({
+                name: newid,
+                date: newdate,
+                start_time: starttime,
+                end_time: endtime,
             });
-
-            this.new_meeting_id = ""
-            this.new_meeting_date = null
-            this.new_meeting_start_time = null
-            this.new_meeting_end_time = null
-       },
-       deletepersonalmeeting: function(deletedmeeting) {
-           var user = this.getselectedperson();
-           user.pmeetings.splice(user.pmeetings.indexOf(deletemeeting), 1)
-       },
-       addnewuser: function() {
-           this.personalmeetings.push({
-               id: this.new_user_id,
-               pmeeting: []
+        },
+        deletepersonalmeeting: function(userid, deletedmeeting) {
+            var user = this.getselectedperson(userid);
+            user.meetings.splice(user.meetings.indexOf(deletedmeeting), 1);
+        },
+        addnewuser: function(newuserid) {
+           this.users.push({
+               user_id: newuserid,
+               meetings: []
            });
-
-           this.new_user_id = "";
-       },
-       deleteselecteduser: function() {
-           var user = this.getselectedperson();
-           selectedperson = "";
-           this.personalmeetings.splice(this.personalmeetings.indexOf(user), 1);
-       },
+        },
+        deleteselecteduser: function(deleteduserid) {
+           this.users.splice(this.users.indexOf(this.getselectedperson(deleteduserid)), 1);
+        },
    },
-
-});
-
+   
+})
