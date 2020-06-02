f551caec16dffb113696861296014d9c173b308e
Mitorah <leinonen.op@gmail.com>
2020-06-02 19:29:59 +0300

    fixed bug.

--- vue-tutorials/src/components/randomnumberbutton.vue
+++ vue-tutorials/src/components/randomnumberbutton.vue
@@ -17,7 +17,7 @@ export default {
     },
     methods: {
         generateRandomNumber() {
-            this.randomGeneratedNumber = Math.floor((Math.random() * 101)) - 50;
+            this.randomGeneratedNumber = Math.floor((Math.random() * 100)) - 50;
         }
     },
     computed: {
