4fc203e0e207c57d08cc4759113754b8f011cddc
Mitorah <leinonen.op@gmail.com>
2020-06-06 20:46:43 +0300

    Removed all old tutorials.

--- Old_tutorials/Programming_tutorials/javascript_info_tutorials/variables/str_q.js
+++
@@ -1,5 +0,0 @@
-let name = "Dilledong!";
-
-alert(`Hello $[1]`); 
-alert(`Hello ${"name"}`);
-alert(`Hello ${name}`);
--- Old_tutorials/Programming_tutorials/w3school_examples/demo_events.js
+++
@@ -1,9 +0,0 @@
-var events = require('events');
-var eventEmitter = new events.EventEmitter();
-
-var myEventHandler = function () {
-    console.log("I hear a scream.");
-}
-
-eventEmitter.addListener('scream', myEventHandler);
-eventEmitter.emit('scream');
