0135d3eeebf7841581569bf4d64c4604dfb44d88
opj <leinonen.op@gmail.com>
2020-07-14 20:19:57 +0300

    Added new test.

--- vue-tutorials/src/sorting-exercises/components/__tests__/unsortedarraytests.js
+++ vue-tutorials/src/sorting-exercises/components/__tests__/unsortedarraytests.js
@@ -20,4 +20,27 @@ describe('Unsorted array tests', () => {
 
         expect(wrapper.vm.unSortedArray.length).toBe(25)
     })
+
+    it('Create array of certain size', () => {
+        for (let i = 1; i < 50; i++) {
+            expect(wrapper.vm.createUnsortedArray(i).length).toBe(i)
+        }
+    })
+
+    // This might not be a good test since it is based in randomness.
+    // In theory, this test can fail, even if the function works properly.
+    it('Scramble array', () => {
+        var newArray = [1,2,3,4,5]
+        var scrambledArray = wrapper.vm.scrambleArray(newArray.slice())
+
+        var numberOfScrambled = 0
+
+        for (let i = 0; i < newArray.length; i++) {
+            if (newArray[i] != scrambledArray[i]) {
+                numberOfScrambled++
+            }
+        }
+
+        expect(numberOfScrambled > 0).toBe(true)
+    })
 })
