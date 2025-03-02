4d5fa2f87df03dedaf6252f0a3463b85b895833e
Mitorah <leinonen.op@gmail.com>
2025-03-02 18:08:10 +0200

    Created tabs and some content for now.

---
+++ src/views/MainWindow.vue
@@ -0,0 +1,40 @@
+<template>
+    <el-container class="bordered">
+        <el-header>
+            {{ title }}
+        </el-header>
+        <el-main>
+            <el-row>
+                <el-col>
+                    <el-card v-for="i in 10" :key=i>
+                        <h1>{{ message }}</h1>
+                    </el-card>
+                </el-col>
+            </el-row>
+        </el-main>
+    </el-container>
+</template>
+
+<script>
+import { loremipsum } from '../utils/loremipsum';
+
+export default {
+    name: 'MainWindow',
+    data() {
+        return {
+        };
+    },
+    computed: {
+        title() {
+            return 'Main Window';
+        },
+        message() {
+            return loremipsum();
+        }
+    }
+};
+</script>
+
+<style>
+@import '../styles/styles.css';
+</style>
