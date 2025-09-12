b7ebc6063344f0956c3480d68f470cf446fac8a4
Olli Leinonen <leinonen.op@gmail.com>
2025-09-12 16:35:01 +0300

    Modified menu button

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,8 @@
   <el-container style="border: 1px solid #ebeef5;">
     <el-header>
       <el-row type="flex" align="middle" style="width: 100%; position: relative;">
-  <el-button @click="onMenuClick" style="margin-left: 0; margin-right: 16px; background: transparent; border: none; box-shadow: none; display: flex; align-items: center; justify-content: center;">
-          <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <rect y="5" width="22" height="2" rx="1" fill="#606266"/>
-            <rect y="10" width="22" height="2" rx="1" fill="#606266"/>
-            <rect y="15" width="22" height="2" rx="1" fill="#606266"/>
-          </svg>
+        <el-button @click="onMenuClick" style="margin-left: 0; margin-right: 16px; background: transparent; border: none; box-shadow: none; display: flex; align-items: center; justify-content: center;">
+          <el-icon><Menu /></el-icon>
         </el-button>
         <span style="position: absolute; left: 0; right: 0; margin: auto; text-align: center; width: 100%; pointer-events: none; font-weight: bold;">HEADER</span>
       </el-row>
@@ -50,14 +46,19 @@
 <script>
 //import MainWindow from './views/MainWindow.vue'
 //import AIWindow from './views/AIWindow.vue'
+
 import Recipes from './views/Recipes.vue'
+import { Menu } from '@element-plus/icons-vue'
+import { ElIcon } from 'element-plus'
 
 export default {
   name: 'App',
   components: {
     // MainWindow,
     // AIWindow,
-    Recipes
+    Recipes,
+    Menu,
+    ElIcon
   },
   data() {
     return {
