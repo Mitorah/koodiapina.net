1ae6c56f891566f4f21e20578eb2abf62edd3d51
Olli Leinonen <leinonen.op@gmail.com>
2025-09-12 16:25:32 +0300

    Moved side menu under a button.

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,42 @@
 <template>
   <el-container style="border: 1px solid #ebeef5;">
     <el-header>
-      {{'HEADER'}}
+      <el-row type="flex" align="middle" style="width: 100%; position: relative;">
+        <el-button icon="el-icon-menu" circle @click="onMenuClick" style="margin-left: 0; margin-right: 16px;" />
+        <span style="position: absolute; left: 0; right: 0; margin: auto; text-align: center; width: 100%; pointer-events: none; font-weight: bold;">HEADER</span>
+      </el-row>
     </el-header>
     <el-main style="border: 1px solid #ebeef5;">
-      <el-tabs v-model="activeTab" tab-position="left" class="demo-tabs">
-        <el-tab-pane v-for="tab in tabs" :label="tab.label" :name="tab.name" :key="tab.name">
-          <el-container>
-            <component :is="tab.component" />
-          </el-container>
-        </el-tab-pane>
-      </el-tabs>
+      <el-drawer
+        title="Menu"
+        v-model="drawerVisible"
+        direction="ltr"
+        size="240px"
+        @close="drawerVisible = false"
+      >
+          <div style="display: flex; flex-direction: column; width: 100%;">
+            <div
+              v-for="tab in tabs"
+              :key="tab.name"
+              @click="selectTab(tab.name)"
+              :style="{
+                padding: '16px',
+                cursor: 'pointer',
+                fontWeight: activeTab === tab.name ? 'bold' : 'normal',
+                color: activeTab === tab.name ? '#409EFF' : '#333',
+                background: activeTab === tab.name ? '#f5f7fa' : 'transparent',
+                borderRadius: '4px',
+                marginBottom: '4px',
+                textAlign: 'left'
+              }"
+            >
+              {{ tab.label }}
+            </div>
+          </div>
+      </el-drawer>
+      <el-container>
+        <component :is="activeTabComponent" />
+      </el-container>
     </el-main>
   </el-container>
 </template>
@@ -31,6 +57,7 @@ export default {
     return {
       activeTab: 'recipes',
       tabs: [],
+      drawerVisible: false,
     }
   },
   mounted() {
@@ -38,8 +65,23 @@ export default {
       // { label: 'Main Window', name: 'main', component: 'MainWindow' },
       // { label: 'AI Window', name: 'ai', component: 'AIWindow' },
       { label: 'Recipes', name: 'recipes', component: 'Recipes' }
-    ]
+    ];
   },
+  computed: {
+    activeTabComponent() {
+      const tab = this.tabs.find(t => t.name === this.activeTab);
+      return tab ? tab.component : null;
+    }
+  },
+  methods: {
+    onMenuClick() {
+      this.drawerVisible = true;
+    },
+    selectTab(tabName) {
+      this.activeTab = tabName;
+      this.drawerVisible = false;
+    }
+  }
 }
 
 </script>
